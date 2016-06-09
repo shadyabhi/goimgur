6a15915dd97db96307b5b1c3446053225196ca3c
Abhijeet Rastogi <me@abhijeetr.com>
2016-06-09 22:40:37 +0530

    Add test for public API

---
+++ goimgur_test.go
@@ -0,0 +1,24 @@
+package goimgur_test
+
+import (
+	"bytes"
+	"testing"
+
+	"../goimgur"
+)
+
+func TestUploadImage(t *testing.T) {
+	t.Parallel()
+	resp, err := goimgur.UploadImage("test_data/image_test.jpg")
+	if err != nil {
+		t.Fatalf("Error executing uploadImage: %s", err)
+	}
+	// Print body for debugging
+	body := &bytes.Buffer{}
+	_, err = body.ReadFrom(resp.Body)
+	if err != nil {
+		t.Fatalf("Error executing goimgur.UploadImage: %s", err)
+	}
+	resp.Body.Close()
+	t.Logf("Response from uploadImage: %s", resp)
+}
